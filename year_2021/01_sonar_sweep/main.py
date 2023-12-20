4a887705efb647f7fb35b7ea8a244dda72502c70
merchristk <merchrist.kiki@datadoghq.com>
2023-12-20 21:14:38 +0100

    2021 day 01: sonar sweep

--- year_2021/01_sonar_sweep/main.py
+++ year_2021/01_sonar_sweep/main.py
@@ -1,5 +1,23 @@
+# https://adventofcode.com/2021/day/1
 
-def main(filename):
-    with open(filename) as file:
-        for line in file.readlines():
-            ...
+
+def read_inputs(filepath):
+    with open(filepath) as file:
+        return list(map(int, file.read().splitlines()))
+
+
+def count_increase(nums: list[int], sliding_window=1):
+    count = 0
+    last = None
+    for i in range(sliding_window, len(nums) + 1):
+        s = sum(nums[i - sliding_window : i])
+        if last is not None and last < s:
+            count += 1
+        last = s
+    return count
+
+
+def main(filepath):
+    nums = read_inputs(filepath)
+    print("Part 1:", count_increase(nums))
+    print("Part 2:", count_increase(nums, 3))
